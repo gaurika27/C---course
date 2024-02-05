0007ae23628978a91f07c82a98400bf99b6d9a6e
gaurika27 <gaurikasharma27@gmail.com>
2024-02-05 17:43:35 +0530

    Cpp codes

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,19 @@
+ // //Traversing and printing methods
+  // //Method 1
+  // for(int i=0; i<v.size();i++){
+  //   cout<<v[i]<<endl;
+  // }
+  // cout<<endl;
+
+  // //Method 2
+  // vector<int>::iterator it;
+  // for(it=v.begin(); it!=v.end(); it++){
+  //   cout<<*it<<endl;
+  // }
+  // cout<<endl;
+
+  // //Method 3
+  // for(auto element:v){
+  //   cout<<element<<endl;
+  // }
+  // cout<<endl;
