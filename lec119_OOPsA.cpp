4306c9e83b4058fd33934d9e8ef18b081a75708a
gaurika27 <gaurikasharma27@gmail.com>
2024-06-10 10:55:18 +0530

    Refactor comments in lec119_OOPsA.cpp for better clarity and understanding

--- lec119_OOPsA.cpp
+++ lec119_OOPsA.cpp
@@ -1,1 +1,6 @@
-//Object is an entity that has two things associated with it, its state/property and behaviour.
+//Object is an entity that has two things associated with it, its state/property and behaviour. 
+/*
+we want the project to have 0bjects and it works with objects so that the program can be more related to real world. 
+why we need to be more near the real world?: so that its readibility, manageability and extensibility increases.
+therefore, oops is the prgram in which the program revolves around the object.
+*/
